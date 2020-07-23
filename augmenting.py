efa0ed53339eefbc5afa1f2a03a06cd599590388
NILOTPAL MRINAL <nilotpal.avanti@gmail.com>
2020-07-23 23:33:45 +0530

    Create augmenting.py

---
+++ augmenting.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import json, glob
+import requests
+import re
+import tmdb
+from importlib import reload
+from utils import create_directory, update_progress
+
+links = pd.read_csv("links.csv", dtype={"tmdb_id": 'Int64' })
+
+
+links.shapesample_id = links.tmdb_id[0]
+tmdb.movie(sample_id)
